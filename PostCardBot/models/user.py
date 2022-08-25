07c4268a5145d9535c97828d4ed2575a33b16884
Wendirad Demelash <wendiradame@gmail.com>
2022-08-25 20:10:02 +0300

    (feat) Add additional fields for user model

--- PostCardBot/models/user.py
+++ PostCardBot/models/user.py
@@ -11,6 +11,9 @@ class User(DatabaseModel, TelegramUser):
     """User model."""
 
     selected_language = "en"
+    is_admin = False
+    is_superuser = False
+    is_active = False
 
     class Meta:
         collection_name = "user"
@@ -25,10 +28,14 @@ class User(DatabaseModel, TelegramUser):
             "selected_language",
             "is_bot",
             "is_premium",
+            "is_admin",
+            "is_superuser",
+            "is_active",
             "added_to_attachment_menu",
             "can_join_groups",
             "can_read_all_group_messages",
             "supports_inline_queries",
+            "created",
         ]
 
     @property
