806090d846587035673bab436fe60e0ce9d946cd
Wendirad Demelash <wendiradame@gmail.com>
2022-08-25 20:14:42 +0300

    (feat) Implement main menu - Add `about` menu handler - Add `help` menu handler - Add `back` menu handler - Update `/start` command handler - Apply translation on handlers

--- PostCardBot/handlers/main_menu.py
+++ PostCardBot/handlers/main_menu.py
@@ -1,6 +1,7 @@
-# flake8: noqa
-
+# flake8: noqa: W605
 """Main menu handler."""
+
+import enum
 
 import aiogram.utils.markdown as md
 from aiogram import types
@@ -9,19 +10,30 @@ from aiogram.dispatcher.filters import Text
 from PostCardBot.core import config
 from PostCardBot.core.decorators import Handler
 from PostCardBot.core.handlers import BaseHandler
-from PostCardBot.models import User
+from PostCardBot.handlers.admin_panel import AdminPanelHandler
+from PostCardBot.handlers.settings import SettingsHandler
 
 _ = config.i18n.gettext
+__ = config.i18n.lazy_gettext
 
 
-class MainMenuHandler(BaseHandler):
+class MainMenuHandler(BaseHandler):  # noqa: N801
     """Main menu handler."""
 
-    @Handler.message_handler(commands=["start"])
-    async def start(message: types.Message):
-        """Start command handler."""
-        bot = await message.bot.get_me()
+    class Buttons(enum.Enum):
+        """Main menu buttons."""
 
+        SEND_POSTCARD = _("📬 Send postcard")
+        MY_POSTCARDS = _("📎 My postcards")
+        HELP = _("💡 Help")
+        ABOUT = _("📖 About")
+        BACK = _("🔙🏠 Main menu")
+
+    @Handler.message_handler(commands=["start"])
+    async def start(message: types.Message, is_back=False):
+        """Start command handler."""
+
+        bot = await message.bot.get_me()
         welcome_text = _(
             "👋 Hello `{first_name}`\n\nWelcome to ✉️📮 \- {bot_name}\! \- 📬🏠\."
             "\n{bot_link} is a Telegram bot, that provided simple and fun "
@@ -38,29 +50,80 @@ class MainMenuHandler(BaseHandler):
             ),
         )
 
-        markup = types.InlineKeyboardMarkup(resize_keyboard=True)
-        for lang_code, lang_name in config.LANGUAGES:
-            markup.add(
-                types.InlineKeyboardButton(
-                    text=lang_name,
-                    callback_data=f"change_language:{lang_code}",
-                )
+        btn_cls = MainMenuHandler.Buttons
+        button_markup = types.ReplyKeyboardMarkup(
+            resize_keyboard=True, selective=True
+        )
+        button_markup.add(
+            types.KeyboardButton(_(btn_cls.SEND_POSTCARD.value)),
+            types.KeyboardButton(_(btn_cls.MY_POSTCARDS.value)),
+        )
+        button_markup.add(
+            types.KeyboardButton(_(SettingsHandler.Buttons.SETTINGS.value)),
+        )
+        if message.from_user.is_superuser or message.from_user.is_admin:
+            button_markup.add(
+                types.KeyboardButton(
+                    _(AdminPanelHandler.Buttons.ADMIN_PANEL.value)
+                ),
             )
+        button_markup.add(
+            types.KeyboardButton(_(btn_cls.HELP.value)),
+            types.KeyboardButton(_(btn_cls.ABOUT.value)),
+        )
         await message.answer(
-            text=welcome_text,
-            reply_markup=markup,
+            text=welcome_text if not is_back else _("🏠 Main menu"),
+            reply_markup=button_markup,
+            parse_mode="MarkdownV2",
         )
 
-    @Handler.callback_query_handler(Text(startswith="change_language"))
-    async def change_language(callback_query: types.CallbackQuery):
-        """Change language handler."""
+    @Handler.message_handler(Text(equals=__(Buttons.BACK.value)))
+    async def back(message: types.Message):
+        """Back command handler."""
 
-        await User(
-            id=callback_query.from_user.id,
-            selected_language=callback_query.data.split(":")[1],
-        ).save()
-        await callback_query.answer()
-        await callback_query.message.edit_reply_markup(reply_markup=None)
-        await callback_query.message.answer(
-            text=_("Your language preference has been changed.")
+        await MainMenuHandler.start(message, is_back=True)
+
+    @Handler.message_handler(Text(equals=__(Buttons.ABOUT.value)))
+    async def about(message: types.Message):
+        """About command handler."""
+
+        about_message = _(
+            "🅰️🅱️🅾️⛎➕\n\n"
+            "At Backos Technologies we believe that we can bring the greatness"
+            " that our country had in the past by solving social, economic and"
+            " political problems using modern technologies\.\n\n"
+            "This bot is developed by {organazation_link} and is licensed"
+            "under the {license_link} license\.\n\n"
+        ).format(
+            organazation_link=md.link(
+                _("Backos Technologies"), "https://www.backostech.com"
+            ),
+            license_link=md.link(
+                _("Apache License 2.0"),
+                "https://www.apache.org/licenses/LICENSE-2.0",
+            ),
         )
+        await message.answer(text=about_message, parse_mode="MarkdownV2")
+
+    @Handler.message_handler(Text(equals=__(Buttons.HELP.value)))
+    async def help(message: types.Message):
+        """Help command handler."""
+
+        help_message = _(
+            "♓️3⃣🕒🅿️\n\n"
+            "*Send postcard*\n"
+            "Send postcard to your friends\.\n\n"
+            "*My postcards*\n"
+            "Check your postcards\.\n\n"
+            "*Settings*\n"
+            "Change settings\.\n\n"
+            "*Help*\n"
+            "Check available options\.\n\n"
+            "*About*\n"
+            "Check bot information\."
+        )
+
+        if message.from_user.is_superuser or message.from_user.is_admin:
+            help_message += _("\n\n*Admin panel*\n" "Access admin panel\.\n\n")
+
+        await message.answer(text=help_message, parse_mode="MarkdownV2")
