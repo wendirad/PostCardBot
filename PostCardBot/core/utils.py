d756aea04ce07d4252bdd4ddc5059d1421f72269
Wendirad Demelash <wendiradame@gmail.com>
2022-09-03 22:21:59 +0300

    (feat) Set current bot before handlers imported

--- PostCardBot/core/utils.py
+++ PostCardBot/core/utils.py
@@ -1,11 +1,12 @@
 """Utils for PostCardBot."""
 
-from aiogram import Dispatcher
+from aiogram import Bot, Dispatcher
 
 
 def load_handlers(bot, dp):
     """Load all handlers from the handlers directory."""
 
     Dispatcher.set_current(dp)
+    Bot.set_current(bot)
 
     import PostCardBot.handlers  # noqa: F401
